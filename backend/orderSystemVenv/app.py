a1bc156145ccc12850cbe9581b4e2e3834503b39
2**10 <aaron911024@gmail.com>
2024-04-23 13:49:40 +0800

    summary

---
+++ backend/orderSystemVenv/app.py
@@ -0,0 +1,55 @@
+﻿import sys
+sys.path.append("./db")
+
+from flask import Flask, render_template, request
+from flask_cors import CORS
+from db.orderSystemDB import DB
+db = DB()
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        data = request.json
+        username = data.get("username")
+        password = data.get("password")
+        print(username, password)
+        if db.searchUsername(str(username)) == True:
+            if db.checkPassword(username, password) == True:
+                print("Welcome")
+                return ("true")
+            else:
+                print("Wrong password")
+                return ("false")
+        else:
+            print("Wrong account")
+            return ("false")
+    db.close_connection()
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        data = request.json
+        username = data.get("username")
+        password = data.get("password")
+        print(username, password)
+        searchUsernameReturn = db.searchUsername(str(username))
+        if searchUsernameReturn == False:
+            if db.insertUsernameAndPassword(str(username),str(password)) == True:
+                return("true")
+        else:
+            return("false")
+    db.close_connection()
+        
+@app.route('/menu', methods=['GET', 'POST'])
+def menu():
+    if request.method == "POST":   
+        return("123")     
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ backend/orderSystemVenv/db/orderSystemDB.py
@@ -0,0 +1,51 @@
+﻿import sqlite3
+test_dbfile = "orderSystemVenv\db\orderSystemDB.db"
+dbfile = "db\orderSystemDB.db"
+
+class DB():
+    def __init__(self):
+        self.conn = sqlite3.connect(dbfile,check_same_thread=False)
+        self.cursor = self.conn.cursor()
+        
+    def close_connection(self):
+        # 關閉游標和資料庫連接
+        self.cursor.close()
+        self.conn.close()
+        
+    # search for username
+    def searchUsername(self, inputUsername):
+        try:
+            username_rows = self.cursor.execute("SELECT username FROM UserData;")
+            for row in username_rows:
+                for field in row:
+                    if inputUsername == field:
+                        print("Username found")
+                        return True
+            print("Username not found")
+            return False
+        except sqlite3.Error as e:
+            print(e)
+            return e
+        
+    # check for password
+    def checkPassword(self, inputUsername, inputPassword):
+        try:
+            password_rows = self.cursor.execute("SELECT password FROM UserData WHERE username = ?;", (inputUsername,))
+            for row in password_rows:
+                if str(inputPassword) == row[0]:
+                    print("Password found")
+                    return True
+            print("Password not found")
+            return False
+        except sqlite3.Error as e:
+            print(e)
+            return e
+        
+    def insertUsernameAndPassword(self, username, password):
+        try:
+            if self.cursor.execute("INSERT INTO UserData (username, password) VALUES (?, ?);", (username, password)):
+                self.conn.commit()
+                return True
+        except sqlite3.Error as e:
+            print("Error:",e)
+            return e
